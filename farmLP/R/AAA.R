138d1ffba267040f7e69729d8e3c8e2c1af0a60d
iracooke <iracooke@conser15.zoo.cam.ac.uk>
2009-08-25 16:06:17 +0100

    minor change

--- farmLP/R/AAA.R
+++ farmLP/R/AAA.R
@@ -1,15 +1,16 @@
 
 .onLoad <- function(libname,pkgname){
 	require(methods)
-
+	browser()
 	  jloc = system.file("java",package="farmLP")
 	  jpars=c("-Xmx500m",paste(c("-Djava.library.path=",jloc),collapse=""))
-	  .jinit(parameters=jpars) # this starts the JVM
-#	.jpackage(pkgname,jars="*",nativeLibrary=FALSE)
-	jniloc <- system.file("jnilibs", package=pkgname)
-	print(jniloc)
-	print(.jaddLibrary(paste(c(jniloc,"libfarmLP.jnilib"),collapse="/"),TRUE))
-	print(.jaddLibrary("libfarmLP.jnlib",jniloc))
+	print(jpars)
+	.jinit(parameters=jpars) # this starts the JVM
+	.jpackage(pkgname,jars="*",nativeLibrary=FALSE)
+#	jniloc <- system.file("jnilibs", package=pkgname)
+#	print(jniloc)
+#	print(.jaddLibrary(paste(c(jniloc,"libfarmLP.jnilib"),collapse="/"),TRUE))
+#	print(.jaddLibrary("libfarmLP.jnlib",jniloc))
 }
 
 setClass("FarmRepresentation")
