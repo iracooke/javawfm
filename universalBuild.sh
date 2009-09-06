dd33e8ce0073619f6277fa7c35ef8259429e17be
iracooke <iracooke@balangbook.local>
2009-09-06 20:13:10 +0100

    Retagged the 1.0 release to 0.99 because it needed changes to get accepted on CRAN

--- universalBuild.sh
+++ universalBuild.sh
@@ -1,6 +1,7 @@
 pkg=farmR_1.0
 rm -rf /tmp/pkg
 mkdir /tmp/pkg
+R CMD BUILD farmR
 R_ARCH=/i386 R CMD INSTALL -l /tmp/pkg $pkg.tar.gz
 R_ARCH=/ppc R CMD INSTALL -l /tmp/pkg --libs-only $pkg.tar.gz
 tar fvcz $pkg.tgz -C /tmp/pkg `echo $pkg|awk -F_ '{print $1}'`
